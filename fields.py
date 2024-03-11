e30656dfd89ea3697f93903eaa51b642981e44b8
Javad Moradkhah <96623570+JavadMoradkhah@users.noreply.github.com>
2024-03-11 20:40:29 +0330

    feat: inherit from "DateField" and "DateTimeField" classes and add validation

--- fields.py
+++ fields.py
@@ -1,30 +1,47 @@
-import datetime
-import jdatetime
 import pytz
+import jdatetime
 from django.conf import settings
-from rest_framework import serializers
+from rest_framework.serializers import DateField, DateTimeField, ValidationError
 
 timezone = pytz.timezone(settings.TIME_ZONE)
 
+DATE_FORMAT = '%Y/%m/%d'
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
 
-class JalaliDateField(serializers.Field):
-    date_format = '%Y/%m/%d'
+
+class JalaliDateField(DateField):
+
+    datetime_parser = jdatetime.datetime.strptime
 
     def to_internal_value(self, data):
-        date = jdatetime.datetime.strptime(data, self.date_format).date()
-        return date.togregorian()
+        try:
+            date = jdatetime.datetime.strptime(data, DATE_FORMAT).date()
+            return super().to_internal_value(date.togregorian())
+        except ValueError:
+            raise ValidationError([
+                'تاریخ وارد شده نامعتبر است',
+                f'باشد {DATE_FORMAT} تاریخ باید در فرمت'
+            ])
 
     def to_representation(self, value):
-        return jdatetime.date.fromgregorian(date=value).strftime(self.date_format)
+        jalali_date = jdatetime.date.fromgregorian(date=value)
+        return super().to_representation(jalali_date.strftime(DATE_FORMAT))
 
 
-class JalaliDateTimeField(serializers.Field):
-    datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+class JalaliDateTimeField(DateTimeField):
+
+    datetime_parser = jdatetime.datetime.strptime
 
     def to_internal_value(self, data):
-        date = jdatetime.datetime.strptime(data, self.datetime_format).astimezone(timezone)
-        return date.togregorian()
+        try:
+            date = jdatetime.datetime.strptime(data, DATETIME_FORMAT).astimezone(timezone)
+            return super().to_internal_value(date.togregorian())
+        except ValueError:
+            raise ValidationError([
+                'تاریخ و زمان وارد شده نامعتبر میباشد'
+                'تاریخ و زمان باید در فرمت ISO 8601 به همراه timezone باشد'
+            ])
 
     def to_representation(self, value):
         jalali_datetime = jdatetime.datetime.fromgregorian(datetime=value.astimezone(timezone))
-        return jalali_datetime.strftime(self.datetime_format)
+        return super().to_representation(jalali_datetime.strftime(DATETIME_FORMAT))
